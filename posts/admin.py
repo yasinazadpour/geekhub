626d286470fad0aa999d5b2bdd0cdd6f8cb3a1f2
YasinAzadpour <yasinaazadpour@gmail.com>
2022-06-13 18:43:40 +0430

    `Comment` model added

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('user', 'title', 'date', 'is_pub')
     list_filter = ('is_pub',)
     search_fields = ('title', 'slug')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'post')
--- posts/models.py
+++ posts/models.py
@@ -10,6 +10,10 @@ class Post(models.Model):
     slug = models.CharField(_("کد صفحه"), max_length=50, unique=True)
     is_pub = models.BooleanField(_("منتشر شده"), default=False)
 
+    @property
+    def comments(self):
+        return self.post_comments.all()
+
     class Meta:
         verbose_name = _('پست')
         verbose_name_plural = _('پست ها')
@@ -17,3 +21,17 @@ class Post(models.Model):
     def __str__(self):
         return f'{self.title}'
 
+
+class Comment(models.Model):
+    post = models.ForeignKey('posts.Post', verbose_name=_("پست"), on_delete=models.CASCADE, related_name="post_comments")
+    user = models.ForeignKey('accounts.MyUser', verbose_name=_("نویسنده"), on_delete=models.CASCADE)
+    text =  models.TextField(_("متن"), max_length=200)
+    date = models.DateTimeField(_("تاریخ"), auto_now=True)
+    rep_to = models.ForeignKey('self', verbose_name=_("در پاسخ به"), on_delete=models.CASCADE, related_name="responses", null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('نظر')
+        verbose_name_plural = _('نظرات')
+
+    def __str__(self):
+        return f'{self.user.name} > {self.post.title}'
