6a3457e621bbb733ca82547cee44236cbc2da04e
YasinAzadpour <yasinaazadpour@gmail.com>
2022-06-16 15:54:45 +0430

    add static files and custom template tags to render pages

--- personalblog/settings.py
+++ personalblog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'personalblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'web/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'web/static',]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'web/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ posts/templatetags/posts_extra.py
@@ -0,0 +1,52 @@
+from django import template
+from django.utils.timesince import timesince
+
+register = template.Library()
+
+COLORS = ["red",
+    "yellow",
+    "orange",
+    "amber",
+    "lime",
+    "green",
+    "emerald",
+    "teal",
+    "cyan",
+    "sky",
+    "blue",
+    "indigo",
+    "violet",
+    "purple",
+    "fuchsia",
+    "pink",
+    "rose",]
+TUNS = [400, 500, 700, 600, 800, 300]
+
+@register.filter("tsince", is_safe=False)
+def timesince_filter(value, arg=None):
+    """Format a date as the time since that date (i.e. "4 days")."""
+    if not value:
+        return ''
+    try:
+        if arg:
+            return timesince(value, arg, depth=1)
+
+        return timesince(value, depth=1)
+        
+    except (ValueError, TypeError):
+        return ''
+
+@register.filter("gencol")
+def gen_color_filter(value):
+    """genrate a color `tailwind css`"""
+    if not value:
+        return 'block'
+    return f"{COLORS[value % 17]}-{TUNS[value % 6]}"
+
+@register.filter("pg")
+def paginator_filter(value, arg='+'):
+    res = list(range(value+1, value+5))
+    if arg=='-':
+        res2 = list(range(value-4, value))
+        return list(zip(res,res2))
+    return res
