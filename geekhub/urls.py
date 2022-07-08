8aede06e7eb1a774b0619dbab2563fe131dd9383
YasinAzadpour <yasinaazadpour@gmail.com>
2022-07-08 14:05:53 +0430

    change the project name

--- geekhub/urls.py
+++ geekhub/urls.py
@@ -1,4 +1,4 @@
-"""personalblog URL Configuration
+"""geekhub URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personalblog.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geekhub.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
